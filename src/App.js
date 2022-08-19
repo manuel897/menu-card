6ffc4f3fc7a5f32b1b22af8bbfb23c9425142e95
Manuel Mangalathu <manuelmangalathu@gmail.com>
2022-08-19 10:18:15 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Menu card</h1>
+            <Card />
+        </div>
+    );
+}
+
+class Card extends React.Component {
+    renderCategory(i) {
+        return <div>this is a category {i}</div>;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderCategory(0)}
+                    {this.renderCategory(1)}
+                    {this.renderCategory(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderCategory(3)}
+                    {this.renderCategory(4)}
+                    {this.renderCategory(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderCategory(6)}
+                    {this.renderCategory(7)}
+                    {this.renderCategory(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
