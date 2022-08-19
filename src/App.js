3ee1319930e741a17dfdd5c4bd04381132ea69d3
Manuel Mangalathu <manuelmangalathu@gmail.com>
2022-08-19 11:33:09 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
+import data from './data';
 
 function App() {
     return (
@@ -13,28 +14,70 @@ function App() {
 }
 
 class Card extends React.Component {
-    renderCategory(i) {
-        return <div>this is a category {i}</div>;
-    }
+    // renderCategory(category) {
+    //     return (
+    //         <div>
+    //             <h3>Category</h3>
+    //             <div>this is a category {category.name}</div>
+    //         </div>
+    //     );
+    // }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderCategory(0)}
-                    {this.renderCategory(1)}
-                    {this.renderCategory(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderCategory(3)}
-                    {this.renderCategory(4)}
-                    {this.renderCategory(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderCategory(6)}
-                    {this.renderCategory(7)}
-                    {this.renderCategory(8)}
-                </div>
+                <Category category={data[0]} />
+                <Category category={data[1]} />
+            </div>
+
+            // { <div>
+            //     <div className="board-row">
+            //         {this.renderCategory(categories[0])}
+            //         {this.renderCategory(1)}
+            //         {this.renderCategory(2)}
+            //     </div>
+            //     <div className="board-row">
+            //         {this.renderCategory(3)}
+            //         {this.renderCategory(4)}
+            //         {this.renderCategory(5)}
+            //     </div>
+            //     <div className="board-row">
+            //         {this.renderCategory(6)}
+            //         {this.renderCategory(7)}
+            //         {this.renderCategory(8)}
+            //     </div>
+            // </div> }
+        );
+    }
+}
+
+class Category extends React.Component {
+    render() {
+        // let itemList = this.props.category.items.map((item, index) => {
+        //     return <Item value={item} />;
+        // });
+        return (
+            <div>
+                <h3>{this.props.category.name}</h3>
+                <ul>
+                    {this.props.category.items.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Item value={item} />{' '}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Item extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.value.name} - {this.props.value.price}
             </div>
         );
     }
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+        name: 'smoothie',
+        items: [
+            {
+                name: 'orange smoothie',
+                price: 80,
+            },
+            {
+                name: 'pineapple smoothie',
+                price: 80,
+            },
+            {
+                name: 'pineapple orange',
+                price: 80,
+            },
+            {
+                name: 'mango smoothie',
+                price: 80,
+            },
+        ],
+    },
+    {
+        name: 'shakes',
+        items: [
+            {
+                name: 'chocolate',
+                price: 80,
+            },
+            {
+                name: 'vanilla',
+                price: 70,
+            },
+            {
+                name: 'Sharjah',
+                price: 60,
+            },
+            {
+                name: 'mango smoothie',
+                price: 80,
+            },
+        ],
+    },
+    {
+        name: 'Fruit shakes',
+    },
+    {
+        name: 'Special shakes',
+    },
+    {
+        name: 'Ice cream scoops',
+    },
+    {
+        name: 'Snacks',
+    },
+];
+
+export default data;
