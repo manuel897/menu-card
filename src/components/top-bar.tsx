b05b2f6346210b4dc321897a41a1e15d59876719
Manuel Mangalathu <manuelmangalathu@gmail.com>
2025-03-24 21:56:52 +0100

    use link

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,15 +1,21 @@
 import '@/styles/app.css';
-import Logo from '@/components/logo';
+import Link from 'next/link';
 
 const TopBar = () => (
   <header className="top-bar w-full top-0 left-0 shadow-md">
     <div className="mx-auto flex justify-between items-center p-2 pl-4 pr-4">
       <div className="flex items-center space-x-1">
-        <img src="/lords-logo.png" alt="Logo" className="h-20 w-auto" />
+        <Link href="/">
+          <img
+            src="/lords-logo.png"
+            alt="Logo"
+            className="h-20 w-auto cursor-pointer"
+          />
+        </Link>{' '}
       </div>
       <nav className="flex space-x-6">
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
       </nav>
     </div>
   </header>
