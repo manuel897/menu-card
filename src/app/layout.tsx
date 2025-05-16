8976cba932d13252b9349688174a25b6614c4d64
Manuel Mangalathu <manuelmangalathu@gmail.com>
2025-05-16 22:46:46 +0200

    rmove  generated cpde

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import TopBar from '@/components/top-bar';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 const a: string = process.env.METADATA_TITLE_1 ?? 'Java';
 const b: string = process.env.METADATA_TITLE_2 ?? 'Shop';
@@ -33,12 +22,10 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/public/apple-touch-icon.png"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <TopBar />
         {children}
       </body>
